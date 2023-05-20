3e0a19108a0e262fb86b8a7f539e215379d79f47
Anıl Gülbaş <gulbasanil92@gmail.com>
2023-05-20 13:21:48 +0300

    Us al guncelle

---
+++ Hafta02/UsAlRecursive/README.md
@@ -0,0 +1,2 @@
+Taban ve üs değerleri kullanıcıdan alınan üs alma işlemini "Recursive" metot ile tamamlayan program.
+
--- Hafta02/UsAlRecursive/src/Main.java
+++ Hafta02/UsAlRecursive/src/Main.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        //Input'ların alındığı yer.
         Scanner sc = new Scanner(System.in);
         System.out.print("Taban değerini giriniz: ");
         int base = sc.nextInt();
         System.out.print("Üs değerini giriniz: ");
         int exponent = sc.nextInt();
 
+        //Fonksiyona gönderilen yer.
         int result = power(base, exponent);
         System.out.println(base + "^" + exponent + " = " + result);
     }
 
+
+    //2^5 örneğini verdiğimizde 2'yi 5 kere çarptıktan sonra döngünün 0 olmasıyla birlikte,
+    //2*2*2*2*2 değerini 1 ile çarparak değeri döndürür.
     public static int power(int base, int exponent) {
         if(exponent == 0) {
             return 1;
